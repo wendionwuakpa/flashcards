482c984060a7b6a631ccd010a1133a520711f180
Wendi O <onwuakpa.c@northeastern.edu>
2024-03-11 00:40:01 -0400

    finished flashcard

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Flashcard from './Components/Flashcard';
 import './App.css'
 
-function App() {
 
-  words_deck = [
-    {'Kedu?' : 'How at you?'}, 
-    {'Kedu?' : 'How at you?'}, 
-  ]
+const words_deck = [
+  {'Kedu?' : 'How are you?'}, 
+  {'Dalụ' : 'Well done, Keep it up'}, 
+  {'Nnọọ' : 'Hello'},
+  {'Imela' : 'Thank you'},
+  {'Kachifo' : 'Goodbye'},
+  {'Biko' : 'Please'},
+  {'Ezigbo mmadu' : 'Good person'},
+  {'Ndo' : 'Sorry'},
+  {'Gini bu aha gi?' : 'What is your name?'},
+  {'Aha m bu' : 'My name is'}
+]
+
+function App() {
+  const [currIndex, setCurrIndex] = useState(0);
+ 
+  const nextCard = () => {
+   const randIndex = Math.floor(Math.random() * words_deck.length)
+   setCurrIndex(randIndex);
+  };
 
   return (
     <>
@@ -18,11 +32,16 @@ function App() {
         </h1>
       </div>
       <h2>
-        How good is your Igbo? Learn some common words and phrases in Nigerian Igbo language and test your vocabulary! 
+        How good is your Igbo? 
       </h2>
-      <p>
-        The number of cards in deck {}
-      </p>
+      <h3>Learn some common words and phrases in Nigerian Igbo language and test your vocabulary! </h3>
+      <p>There are {words_deck.length} cards in deck</p>
+
+      <Flashcard wordPair={words_deck[currIndex]}/>
+      <button type='button' className='nextCard' onClick={nextCard}>
+        Next Card 
+        ⭢
+      </button>
     </>
   )
 }
